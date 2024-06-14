403741e4deee086fb5118ec3cf333d14150c257b
anura1402 <ann99909@gmail.com>
2024-06-14 16:37:35 +0300

    Adding navigation

--- app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.composition.R
 import com.example.composition.databinding.FragmentChooseLevelBinding
 import com.example.composition.databinding.FragmentGameFinishedBinding
+import com.example.composition.domain.entity.Level
 
 
 class ChooseLevelFragment : Fragment() {
@@ -15,20 +16,43 @@ class ChooseLevelFragment : Fragment() {
     private var _binding: FragmentChooseLevelBinding? = null
     private val binding: FragmentChooseLevelBinding
         get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentChooseLevelBinding.inflate(inflater,container,false)
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            buttonLevelEasy.setOnClickListener { launchGameFragment(Level.EASY) }
+            buttonLevelTest.setOnClickListener { launchGameFragment(Level.TEST) }
+            buttonLevelNormal.setOnClickListener { launchGameFragment(Level.NORMAL) }
+            buttonLevelHard.setOnClickListener { launchGameFragment(Level.HARD) }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun launchGameFragment(level: Level) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(GameFragment.NAME)
+            .commit()
+    }
+
+    companion object {
+
+         const val NAME = "ChooseLevelFragment"
+        fun newInstance(): ChooseLevelFragment {
+            return ChooseLevelFragment()
+        }
     }
 }
